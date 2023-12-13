3c8728c3877a24df010ac15d99e146f26097a888
ferpven <ferpven@gmail.com>
2023-12-13 16:15:11 +0300

    Updated to Net 8.0
    When we downgraded the version, I couldn't run it on my machine.

--- MyLibrary/obj/Debug/net7.0/MyLibrary.AssemblyInfo.cs
+++ MyLibrary/obj/Debug/net7.0/MyLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052a0002b7977bd1188108c83163116f51062249")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
